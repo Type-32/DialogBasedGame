d51ef9487e5711e001844c979d83db808dc4c98e
Type-32 <wilsonsu2008@hotmail.com>
2023-10-31 21:15:14 +0800

    Added so much stuff and finished it

--- dialogue.py
+++ dialogue.py
@@ -1,6 +1,149 @@
 import string
+import asyncio
+import os
+import sys
+import random
+
 
 class Dialogue:
-    def __init__(self, ask, responses):
+    def __init__(self, ask, responses, actions: list[callable] = None):
+        # The constructor. Initializes the objects of this class's instance
         self.ask = ask
         self.responses = responses
+        self.actions = actions
+
+    def fetch(self, delay: float = 1, afterDelay: float = 0, optionDelay: float = 1, framed: bool = True, clearScreenBefore: bool = False, setLowerCase: bool = True):
+        result = ""
+        if clearScreenBefore:
+            clearScreen() # Clears screen before printing the prompt and answers.
+        if framed:
+            delayedPrint("-" * (len(self.ask) + 20), 0) # Prints out the top separator of the prompt.
+            delayedPrint(f"     {self.ask}     ", delay) # Prints out the prompt with its respective margin.
+            delayedPrint(("-" * (len(self.ask) + 20)) + "\n", 0, afterDelay) # Prints out the bottom separator.
+        else:
+            delayedPrint(f"{self.ask}", delay, afterDelay) # Prints only the prompt.
+        for i in range(len(self.responses)):
+            delayedPrint(f"{chr(97+i).upper()}. " + self.responses[i], optionDelay) # Prints out the available answer choices.
+        while True:
+            result = input(">> ") # Detects user input.
+
+            # Check for Validity:
+            # if the answer is empty or out of bounds of available answers, the function re-prompts the user for an answer.
+            if result == "":
+                continue
+            if (ord('a') - ord(result.lower()[0])) <= len(self.responses):
+                result = result.upper()[0] if not setLowerCase else result.lower()[0]
+                break
+        return result # returns a result, either the letter of the choice or the capital letter of the choice.
+
+    def execute(self, delay: float = 1, afterDelay: float = 0, optionDelay: float = 1, framed: bool = True, clearScreenBefore: bool = False, setLowerCase: bool = True):
+        ans = ord('a') - ord(self.fetch(delay, afterDelay, optionDelay, framed, clearScreenBefore, setLowerCase).lower())
+        print(ans)
+        self.actions[ans].__call__()
+
+    def determine(self):
+        ans = (ord('a') - ord(self.fetch(True)[0]))
+        for i in range(len(self.dialogues)):
+            if i == ans:
+                return self.dialogues[i]
+        return -1
+
+
+def pause(): # A simple pause function
+    input(processStyle("Press return to continue...", style_name='italic'))
+
+def delayedPrint(content: string, delay: float = 0, afterDelay: float = 0, returnLine: bool = True):
+    asyncio.run(__delayedPrint__(content, delay, afterDelay, returnLine)) # uses Asyncio to run the asynchronous function. mimics the effects of sys.sleep(0)
+
+
+def loader(duration: float = 2, placeholder: string = "Processing"):
+    asyncio.run(__loaderText__(duration, placeholder)) # uses Asyncio to run the asynchronous function. mimics the effects of sys.sleep(0)
+
+
+def clearScreen():
+    # if the Operating System is Windows
+    if sys.platform == "win32":
+        os.system("cls")
+    # if the Operating System is Linux or MacOS
+    if sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin":
+        os.system("clear") # Clears the terminal screen.
+
+# The core function of delayedPrint().
+async def __delayedPrint__(content: string, delay: float, afterDelay: float, returnLine: bool):
+    temp = float(delay / len(content)) # Calculates the duration needed for pause between printing out each letter.
+    for i in content: # Prints out the content letter by letter.
+        print(i, end='')
+        await asyncio.sleep(temp) # Uses an asynchronous await to sleep. similar to sys.sleep().
+    if returnLine:
+        print() # Returns the Line.
+    await asyncio.sleep(afterDelay) # Delays the function after printing out the content. For custom purposes.
+
+# The core function of loader().
+async def __loaderText__(duration: float, placeholder: string):
+    for _ in range(3):  # Adjust the range for the desired number of cycles
+        for dot_count in range(4):  # The number of dot_count will cycle from 0 to 3, with each index representing a character in a loop
+            print(f'\r{placeholder}...  [ {__loaderSpinner__(dot_count)} ]', end='')
+            await asyncio.sleep(duration / 12)
+
+
+# The function that returns the characters representing the indexes in the spinner.
+def __loaderSpinner__(index: int):
+    temp = index % 4
+    return processStyle("|" if temp == 0 else "/" if temp == 1 else "-" if temp == 2 else "\\", 'aqua', 'bold')
+
+
+#I just wanted to make stuff look cooler since we don't have the time to make fabulous UI
+# ansi color codes for the color parameter in processStyle()
+color = {
+    'red': '\033[31m',
+    'blue': '\033[34m',
+    'aqua': '\033[36m',
+    'green': '\033[32m',
+    'dark_green': '\033[38;5;2m',
+    'yellow': '\033[33m',
+    'orange': '\033[38;5;202m',
+    'purple': '\033[35m',
+    'light_purple': '\033[38;5;93m',
+    'dark_purple': '\033[38;5;54m',
+    'gray': '\033[37m',
+    'light_gray': '\033[38;5;250m',
+    'dark_blue': '\033[38;5;18m'
+}
+
+# ansi style codes for the style parameter in processStyle()
+style = {
+    'bold': '\033[1m',
+    'italic': '\033[3m',
+    'underlined': '\033[4m'
+}
+
+
+# Takes in a string, processes it into a colored or style formatted string, then returns it
+def processStyle(content: str, color_name=None, style_name=None):
+    color_code = color[color_name] if color_name else ''
+    style_code = style[style_name] if style_name else ''
+    # Reset code
+    reset = '\033[0m'
+    return f"{color_code}{style_code}{content}{reset}"
+
+
+# Unused function that obfuscates the text, but was never used
+def obfuscate(text):
+    return ''.join(random.choice(string.printable) for _ in text)
+
+
+# Unused Code
+
+# I deleted this because this was extremely experimental
+# def print_obfuscated(text):
+#     obfuscated = obfuscate(text)
+#     print(obfuscated, end='\r')
+#     sys.stdout.flush()
+#     time.sleep(1)
+#
+#     revealed = list(obfuscated)
+#     for i in range(len(text)):
+#         revealed[i] = text[i]
+#         print(''.join(revealed), end='\r')
+#         sys.stdout.flush()
+#         time.sleep(0.5)  # Adjust this to change the speed of revealing
--- main.py
+++ main.py
@@ -1,50 +1,161 @@
-import pygame
+from dialogue import Dialogue
+import dialogue as dl
+import sys
 
-pygame.init()
-screen = pygame.display.set_mode((600, 500))
-clock = pygame.time.Clock()
+# Initialization
+money: int = 100
+choices: int = 0
 
-box = pygame.Rect(300, 200, 100, 100)
-player = pygame.Rect(50, 50, 30, 30)
-
-font = pygame.font.SysFont('Times New Roman', 50)
-texts = ['Hi', 'Hello', 'Who are ye?', 'Someone']
-text_renders = [font.render(text, True, (0, 0, 255)) for text in texts]
-
-index = -1
-space_released = True
-
-while True:
-    clock.tick(60)
-    screen.fill((0, 0, 0))
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        player.x -= 3
-    if keys[pygame.K_d]:
-        player.x += 3
-    if keys[pygame.K_s]:
-        player.y += 3
-    if keys[pygame.K_w]:
-        player.y -= 3
-
-    pygame.draw.rect(screen, (0, 255, 0), box, width=2)
-    pygame.draw.rect(screen, (255, 0, 0), player)
-
-    if player.colliderect(box):
-        if keys[pygame.K_SPACE] and space_released:
-            space_released = False
-            index = (index + 1) if (index + 1) != len(text_renders) else 0
-        elif not keys[pygame.K_SPACE]:
-            space_released = True
+# Initialize Dialogue
+dl.delayedPrint(f"Currently, you have {money} units of money. Your choices result in the loss or gain of your money.", 3, 1)
+dl.delayedPrint(dl.processStyle("Choose Wisely.", 'red', 'italic'), 2, 1)
+dl.loader(3)
+dl.clearScreen()
+dl.delayedPrint("Enter your floor number for morning registration: ", returnLine=False)
+floorNum: int = int(input())
+if 8 >= floorNum >= 5:
+    temp1: Dialogue = Dialogue("Your form room teacher is already gone.",
+        [
+            "Give Mr Mobsby 50 units of money and let him to make up the MR",
+            "Give PSO 25 units of money and let him to make up the MR",
+            "Do nothing"
+        ]
+    )
+    returned = temp1.fetch(1.5)
+    dl.clearScreen()
+    if returned == 'a':
+        dl.delayedPrint("You have used 50 units of money to let Mr. Mobsby make up the MR for you.", 3, 1)
+        dl.delayedPrint("He gives back you the money and says: ", 1.5, 0.5, returnLine=False)
+        dl.delayedPrint(dl.processStyle("I am not in short of money.", style_name='italic'), 1, 1)
+        dl.delayedPrint(dl.processStyle("He then gives you back your money and helped you make up your MR.", 'green'), 2.5, 1)
+    elif returned == 'b':
+        dl.delayedPrint("You have used 25 units of money to let PSO make up the MR for you.", 3, 1)
+        dl.delayedPrint("The PSO helped you make up your MR.", 1.5, 1)
+        money -= 25
     else:
-        index = -1
+        dl.delayedPrint(
+            "Your form room teacher has found out that you didn't come for MR, so he took away 30 units of your money.",
+            3, 1)
+        money -= 30
+    choices += 1
+elif 5 > floorNum > 0:
+    dl.delayedPrint(dl.processStyle("You've survived from the MR in the morning.", 'green'), 2, 2)
+elif floorNum <= 0:
+    temp1: Dialogue = Dialogue("You suddenly feel an immense force of gravity pulling you downwards, as if the floor of the elevator collapsed.",[
+        "Try to hold on to something",
+        "Scream for help"
+    ])
+    dl.delayedPrint("When going down the staris, you noticed an elevator that seemingly appeared out of nowhere in the wall of the stairwell.\nYou entered and messed around with the elevator buttons.", 2, 0.5)
+    dl.delayedPrint("You didn't notice that the floor number of the elevator kept decreasing below 0.", 3.5, 1)
+    dl.delayedPrint("The noise of the running elevator slowly became overwhelming.\nYou noticed that the elevator is beginning to shake violently.\n", 2, 1)
+    temp1.fetch(3.5, 2, framed=False)
+    dl.clearScreen()
+    dl.delayedPrint("You tried...", 1, 0.5, returnLine=False)
+    dl.delayedPrint(" But your efforts were in vain.", 3.5, 0.5)
+    dl.delayedPrint("As you watched the world fade from your eyes, you felt a sudden 'thud' on your back.", 2.5, 1)
+    dl.delayedPrint("You've entered... ", 1, 0.5, returnLine=False)
+    dl.delayedPrint(dl.processStyle("The Backrooms.", 'yellow', 'bold'), 3, 2)
+    dl.delayedPrint(dl.processStyle("error error error error failed context fetching error error error error unauthorized access to simulation error error error error", 'red', 'underlined'), 1, 0)
+    dl.clearScreen()
+    sys.exit()
+else:
+    dl.clearScreen()
+    dl.delayedPrint(dl.processStyle("What were you even doing there?", 'red', 'underlined'), 3, 1)
+    sys.exit()
 
-    if index != -1:
-        screen.blit(text_renders[index], (0, 0))
+print()
+dl.delayedPrint(f"You currently have {dl.processStyle(str(money), color_name='yellow', style_name='bold')} units of money.", 2, 1)
+dl.loader(3, "Loading Dialogues")
+dl.clearScreen()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
+dl.delayedPrint(f"Input the floor of your {dl.processStyle("first", 'blue')} class: ", returnLine=False)
+class1 = int(input())
+dl.delayedPrint(f"Input the floor of your {dl.processStyle("second", 'yellow')} class: ", returnLine=False)
+class2 = int(input())
+dl.delayedPrint(f"Input the floor of your {dl.processStyle("third", 'purple')} class: ", returnLine=False)
+class3 = int(input())
 
-    pygame.display.update()
+dif1 = abs(class1 - class2)
+dif2 = abs(class2 - class3)
+dif3 = abs(floorNum - class1)
+
+if dif1 + dif2 + dif3 + class3 > 15:
+    dl.delayedPrint(f"Since there's too much stairs for you to climb, you have been deducted {dl.processStyle("20 units of money.", 'orange')}", 3, 1)
+    money -= 20
+else:
+    dl.delayedPrint(dl.processStyle("You have survived from climbing the stairs in the morning!", 'green'), 3, 1)
+
+print()
+dl.delayedPrint(f"You currently have {dl.processStyle(str(money), color_name='yellow', style_name='bold')} units of money.", 2, 1)
+dl.loader(3, "Loading Dialogues")
+dl.clearScreen()
+
+lunch: Dialogue = Dialogue(f"Now that you've finished your last class of the day, choose a way to {dl.processStyle("have", 'blue')}/{dl.processStyle("obtain", 'orange')} your lunch:",
+   [
+       "Steal other students' takeaway",
+       "Order food takeaway",
+       "Eat in the school canteen"
+   ]
+)
+
+lunchAns: chr = lunch.fetch(2, 0.5, 1.5)
+if lunchAns == 'a':
+    dl.delayedPrint("You stole a student's lunch...", 2, 1, False)
+    dl.delayedPrint(dl.processStyle(" but Mr. Mobsby caught you.", 'red', 'italic'), 3, 1)
+    dl.delayedPrint(dl.processStyle("You received a detention from Mr. Mobsby and was taken 50 units of money.", 'red'), 3, 1)
+    money -= 50
+elif lunchAns == 'b':
+    dl.delayedPrint("You ordered a takeaway. However, a student had stole it from you. Because of it, you lost 50 units of money.", 4, 2)
+    dl.delayedPrint(dl.processStyle("Luckily, Mr. Mobsby felt empathy for you and shared his lunch and some money with you. You gained 50 units of money.", 'green'), 4, 1)
+    money += 50
+else:
+    dl.delayedPrint("You paid 50 units of money for the food in the canteen.", 2, 1)
+    dl.delayedPrint(f"However, the food caused you a stomachache and you had to take care of it by paying {dl.processStyle("50 more units of money", 'orange')} by receiving medicine from the infirmary.",6, 2)
+    money -= 10
+choices += 1
+
+print()
+dl.delayedPrint(f"You currently have {dl.processStyle(str(money), color_name='yellow', style_name='bold')} units of money.", 2, 1)
+dl.loader(3, "Loading Dialogues")
+dl.clearScreen()
+
+night: Dialogue = Dialogue(
+    "The school is over. Now it is time for night study.",
+    [
+        "Escape night study",
+        "Study Attentively"
+    ]
+)
+nightStudy = night.fetch(2, optionDelay=2)
+if nightStudy == 'a':
+    dl.clearScreen()
+    activity: Dialogue = Dialogue(
+        "You've escaped night study. You can now do some activities.",
+        [
+            "Play Football",
+            "Exercise in the Gym"
+        ]
+    )
+    act = activity.fetch(2, optionDelay=1)
+    if act == 'a':
+        dl.delayedPrint("Sadly, you've been caught by Mr. Mobsby. That's 50 points off for you.", 2, 1)
+    else:
+        dl.delayedPrint("Your PE Teacher found you and encouraged you because you got U in your last fitness test. Extra 60 points for you.", 3, 1)
+    choices += 1
+else:
+    dl.delayedPrint("Sadly, your inward curling action is discovered by your friends. Minus 10 points.", 2, 1)
+choices += 1
+
+print()
+dl.delayedPrint(f"You currently have {dl.processStyle(str(money), color_name='yellow', style_name='bold')} units of money.", 2, 1)
+dl.loader(5, "Processing Results")
+dl.clearScreen()
+
+if money > 0:
+    dl.delayedPrint(f"You have survived SCIE! Your current amount of money is {dl.processStyle(str(money), color_name='green', style_name='bold')}.", 3, 1)
+else:
+    dl.delayedPrint(f"Your total amount of money is {dl.processStyle(str(money), color_name='orange', style_name='bold')}.", 3, 1, False)
+    dl.delayedPrint(dl.processStyle(f" Watch out from now on, since you now owe SCIE {money} units of money.", 'red', 'italic'), 3, 1)
+
+dl.delayedPrint(f"You made {dl.processStyle(str(choices), 'aqua', 'underlined')} big choices in SCIE today.", 3, 1)
+
